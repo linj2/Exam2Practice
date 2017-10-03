4b9b07b1002943d8ec34adc2620ef2a9e3deedaf
Jing Lin <linj2@rose-hulman.edu>
2017-10-02 21:08:04 -0400

    2

--- src/practice_problem2.py
+++ src/practice_problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 2, practice_problem 2.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jing Lin.
+"""  # TODONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -30,7 +30,7 @@ import simple_testing as st
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_practice_problem2a()
+    # run_test_practice_problem2a()
     run_test_practice_problem2b()
 
 
@@ -42,7 +42,7 @@ def main():
 def run_test_practice_problem2a():
     """ Tests the   practice_problem2a  function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # TODONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem2a  function defined below.
     #   Include at least **   4 reasonable   ** tests.
     #
@@ -55,6 +55,41 @@ def run_test_practice_problem2a():
     print('--------------------------------------------------')
     print('Testing the   practice_problem2a   function:')
     print('--------------------------------------------------')
+    # Test 1:
+    print('--------------------------------------------------')
+    print('Test 1')
+    seq = [4, 5, 6, 7, 8]
+    expected = [5, 6, 7, 8, 9]
+    actual = practice_problem2a(seq, 1)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 2:
+    print('--------------------------------------------------')
+    print('Test 2')
+    seq = [1, 1, 1, 1, 1]
+    expected = [6, 6, 6, 6, 6]
+    actual = practice_problem2a(seq, 5)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 3:
+    print('--------------------------------------------------')
+    print('Test 3')
+    seq = [1, 2, 3, 4, 4]
+    expected = [5, 6, 7, 8, 8]
+    actual = practice_problem2a(seq, 4)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 4:
+    print('--------------------------------------------------')
+    print('Test 4')
+    seq = [0, -5, -6, -7, -8]
+    expected = [10, 5, 4, 3, 2]
+    actual = practice_problem2a(seq, 10)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 
 def practice_problem2a(sequence, delta):
@@ -76,7 +111,7 @@ def practice_problem2a(sequence, delta):
       :type delta:    int
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # TODONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
@@ -84,6 +119,9 @@ def practice_problem2a(sequence, delta):
     #    TIME ESTIMATE:   5 minutes.
     ####################################################################
 
+    for k in range(len(sequence)):
+        sequence[k]+= delta
+    return sequence
 
 def run_test_practice_problem2b():
     """ Tests the   practice_problem2b  function. """
@@ -170,14 +208,17 @@ def practice_problem2b(sequence):
       :type sequence [str]
     """
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # TODONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   10 minutes.
     ####################################################################
-
+    new_string = ''
+    for k in range(len(sequence)):
+        new_string += sequence[k][:1]
+    return new_string
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
